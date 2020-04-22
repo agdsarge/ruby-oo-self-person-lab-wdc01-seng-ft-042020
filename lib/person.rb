d7e15e8ba2add671c24be8c08b560dba8619d458
saimaja <saimaakhtar@gmail.com>
2020-04-22 14:25:26 -0400

    saima's commits

--- lib/person.rb
+++ lib/person.rb
@@ -1,20 +1,23 @@
 # your code goes here
 class Person
+    
+    attr_reader :name, :hygiene, :bank_account, :happiness
+    
     def initialize(name)
         @name = name
         @bank_account = 25
-        @happiness_points = 8
+        @happiness = 8
         @hygiene = 8
     end
-    attr_reader :name, :hygiene, :bank_account
+   
 
     def change_happiness(new_score)
         if new_score >= 10
-            @happiness_points = 10
+            @happiness = 10
         elsif new_score <= 0
-            @happiness_points = 0
+            @happiness = 0
         else
-            @happiness_points = new_score
+            @happiness = new_score
         end
     end
 
@@ -32,17 +35,49 @@ class Person
         @bank_account += dollars
     end
 
+    def clean?
+        @hygiene > 7
+    end
 
+    def happy?
+        @happiness > 7
+    end
 
+    def get_paid(salary)
+        self.change_bank_account(salary)
+        "all about the benjamins"
+    end
 
+    def take_bath
+        self.change_hygiene(@hygiene + 4)
+        "♪ Rub-a-dub just relaxing in the tub ♫"
+    end
+
+    def work_out
+        self.change_hygiene(@hygiene -3)
+        self.change_happiness(@happiness + 2)
+        "♪ another one bites the dust ♫"
+    end
+
+   
 
 end
 
 
 bob_dylan = Person.new("bob")
 
-bob_dylan.change_hygiene(12)
-puts bob_dylan.hygiene
+bob_dylan.change_hygiene(2)
+#puts bob_dylan.hygiene
 
 bob_dylan.change_bank_account(100)
 puts bob_dylan.bank_account
+
+puts bob_dylan.happy?
+
+puts bob_dylan.get_paid(2000)
+puts bob_dylan.bank_account
+puts bob_dylan.take_bath
+puts bob_dylan.hygiene
+puts bob_dylan.work_out
+puts bob_dylan.hygiene
+puts bob_dylan.happiness
